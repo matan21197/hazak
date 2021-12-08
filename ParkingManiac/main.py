88dc12128c804dc943082e95c9dae13e450a843b
yonatan edelshtein <yonatan1346@gmail.com>
2021-12-08 15:14:51 +0200

    add DB support

--- ParkingManiac/main.py
+++ ParkingManiac/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, session, redirect, url_for,make_response
 from datetime import timedelta, date
 import os
+import datetime
+import json
+
+from models.models import db
+from models.models import Park, Parkinglot, Point
 
 TEMPLATE_DIR = os.path.abspath('./templates')
 STATIC_DIR = os.path.abspath('./static')
@@ -8,11 +13,26 @@ STATIC_DIR = os.path.abspath('./static')
 # app = Flask(__name__) # to make the app run without any
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/parking.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 @app.route("/")
 def home():
+    point = Point()
+    point.init(position=1,x=1,y=1)
+
+    p = Parkinglot(name='admin2', description="desc2", location="yara2", img="y2")
+    park = Park(available=True, last_updated=datetime.datetime.now(), parkinglot=p)
+    db.session.add(p)
+    db.session.add(park)
+    db.session.commit()
+
     return render_template("index.html")
 
 if __name__ == "__main__":
-	app.config['SESSION_TYPE'] = 'filesystem'
-
-	app.run(debug=True, host='0.0.0.0')
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True, host='0.0.0.0')
---
+++ ParkingManiac/models/models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+import json
+
+class Point:
+    def __init__(self):
+        position = ""
+        x = ""
+        y = ""
+
+    def init(self, position, x, y):
+        position = position
+        x = x
+        y = y
+
+    def init_str(self, str):
+        j = json.loads(str)
+        self.position = j['position']
+        self.x = j['x']
+        self.y = j['y']
+
+    def toJSON(self):
+        return "{ 'position': {}, 'x': {}, 'y': {} }".format(self.position, self.x, self.y)
+    
+class Parkinglot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, index=True, unique=False)
+    description = db.Column(db.Text, index=True, unique=False)
+    location = db.Column(db.Text, index=True, unique=False)
+    img = db.Column(db.Text, index=True, unique=False)
+    points = db.Column(db.Text, index=False, unique=False)
+    parks = db.relationship('Park', backref='parkinglot', lazy=False)
+
+class Park(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    available = db.Column(db.Boolean, index=True, unique=False)
+    last_updated = db.Column(db.DateTime, index=True, unique=False)
+    points = db.Column(db.Text, index=False, unique=False)
+    parkinglot_id = db.Column(db.Integer, db.ForeignKey('parkinglot.id'), nullable=False)
