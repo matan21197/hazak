ac50e522d618440dc21043aa857d35dd8966941f
yaaraeli <awsomepenguin1@gmail.com>
2021-12-09 13:09:20 +0200

    fixed the points in the db

--- ParkingManiac/main.py
+++ ParkingManiac/main.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template, request, jsonify
 import os
 import cv2
 import numpy as np
-from DetectCarParking import detect_car
+#from DetectCarParking import detect_car
+import json
 
 from image_utils import process_image
 from models.models import db, Point, Park
@@ -43,6 +44,8 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    #createParkinglot()
+    randomdb()
     return render_template('beta.html')
 
 
@@ -57,7 +60,7 @@ def index():
 #     emit('get-image', 'static/img/owl1.jpg', broadcast = True)
 
 @app.route("/uploadImage/<name>", methods=['POST'])
-def getImage(name):
+def uploadImage(name):
     global image_counter
     image_counter += 1
 
@@ -87,23 +90,34 @@ def split_into_parking_spots(name):
     return ""
 
 
-@app.route("/initImage/<name>", methods=['POST'])
-def get_parkinglot(name):
+@app.route("/getParks/<name>", methods=['POST'])
+def get_parks(name):
     # select from db
     parkinglot = Parkinglot.query.filter_by(name=name).first()
-    points_array = jsonify([[point.toJSON() for point in park.points] for park in parkinglot])
-    return jsonify(img=parkinglot.img, points=points_array)
+    parks_jsons = []
+    image = '"image: "' + '"' + parkinglot.img + '"'
+    parks_jsons.append(image)
+    for park in parkinglot.parks:
+        print(park.points)
+        a = '{"available":'
+        a += '"{}",{}'.format(park.available, park.points)
+        a += "}"
+        parks_jsons.append(a)
+
+    final_json = '[\n{}\n]'.format(",".join(parks_jsons))
+    print(final_json)
+    return json.dumps(final_json)
 
 
 def create_parking_lot(name, image):
     path = save_image(image, name)
-    p = Parkinglot(name=name, points="", description="desc2", location="location", img=path)
+    p = Parkinglot(name=name, description="desc2", location="location", img=path)
     db.session.add(p)
     db.session.commit()
 
 
 # updates in the db the available parks
-def get_availables(image, name):
+def get_availables(name):
     parking_lot = Parkinglot.query.filter_by(name=name).first()
     image = parking_lot.img
     for park in parking_lot.parks:
@@ -116,14 +130,14 @@ def is_available(image, points):
     img_np = cv2.imread(image)
     processed_image = process_image(img_np, points)
     # Note: image should be numpy array !!!!
-    return detect_car.is_free_parking(processed_image)
+    return True# detect_car.is_free_parking(processed_image)
 
 
 def update_parking_lot(name, image):
     parking_lot = Parkinglot.query.filter_by(name=name).first()
     parking_lot.img = image
     db.session.commit()
-    get_availables(image, name)
+    get_availables(name)
 
 
 """
@@ -140,24 +154,51 @@ def save_image(image: bytes, name: str):
 def getParkingLot(name):
     return Parkinglot.query.filter_by(name=name).first()
 
+def createParkinglot():
+    p = Parkinglot(name='main parking', description="main parking", location="main parking", img="")
+    db.session.add(p)
+    db.session.commit()
+
 def randomdb():
-    point1 = Point().init(position=1, x=2, y=1)
-    point2 = Point().init(position=1, x=1, y=1)
-    point3 = Point().init(position=1, x=3, y=1)
-    point4 = Point().init(position=1, x=4, y=1)
-    point5 = Point().init(position=1, x=5, y=1)
-    point6 = Point().init(position=1, x=6, y=1)
-    point7 = Point().init(position=1, x=7, y=1)
-    point8 = Point().init(position=1, x=8, y=1)
-    points1 = [point1, point2, point3, point4]
-    points1 = [point8, point5, point6, point]
+    point1 = Point()
+    point2 = Point()
+    point3 = Point()
+    point4 = Point()
+    point1.init(position=1, x=2, y=1)
+    point2.init(position=2, x=1, y=1)
+    point3.init(position=3, x=3, y=1)
+    point4.init(position=4, x=4, y=1)
+    """
+    point5 = Point().init(position=1, x=5, y=1).toJson()
+    point6 = Point().init(position=1, x=6, y=1).toJson()
+    point7 = Point().init(position=1, x=7, y=1).toJson()
+    point8 = Point().init(position=1, x=8, y=1).toJson()
+    points2 = [point8, point5, point6, point7]
+    """
+    points1 = [point1.toJson(), point2.toJson(), point3.toJson(), point4.toJson()]
+    print(points1)
+
     p = Parkinglot(name='test', description="desc2", location="yara2", img="y2")
-    park1 = Park(available=True, last_updated=datetime.now(), parkinglot=p, points=jsonify(points1))
-    park2 = Park(available=True, last_updated=datetime.now(), parkinglot=p, points=jsonify(points2))
+
+    park1 = Park(available=True, last_updated=datetime.now(), parkinglot=p, points="".join(points1))
+    #park2 = Park(available=True, last_updated=datetime.now(), parkinglot=p, points=",\n".join(points2))
     db.session.add(p)
     db.session.add(park1)
-    db.session.add(park2)
+    #db.session.add(park2)
     db.session.commit()
+
+    parkinglot = Parkinglot.query.filter_by(name='test').first()
+    parks_jsons = []
+
+    for park in parkinglot.parks:
+        print(park.points)
+        a = '{"available":'
+        a += '"{}",{}'.format(park.available, park.points)
+        a += "}"
+        parks_jsons.append(a)
+    final_json = '[\n{}\n]'.format(",".join(parks_jsons))
+
+
 
 
 if __name__ == "__main__":
