e4e326cec01ff563211849bccd29b9aa4e427156
yonatan edelshtein <yonatan1346@gmail.com>
2021-12-09 09:56:13 +0200

    Update main.py

--- ParkingManiac/main.py
+++ ParkingManiac/main.py
@@ -42,7 +42,7 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('beta.html')
 
 
 # @socket_.on('my_event', namespace='/test')
